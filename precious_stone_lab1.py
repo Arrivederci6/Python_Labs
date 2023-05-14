d19ce6112b82a960e203726fe4717ba2e1c21dd2
Arrivederci6 <mad060605@gmail.com>
2023-05-14 16:32:45 +0300

    lab1 - fixed some issues, added start point.

--- precious_stone_lab1.py
+++ precious_stone_lab1.py
@@ -1,5 +1,6 @@
 class PreciousStone:
     """ A class that represents precious stone"""
+    instance = None
 
     def __init__(self, name, carat, color, clarity, price_per_carat):
         """Initializing objects"""
@@ -9,6 +10,12 @@ class PreciousStone:
         self.__clarity = clarity
         self.__price_per_carat = price_per_carat
     
+    def get_instance():
+        if PreciousStone.instance == None:
+            PreciousStone.instance = PreciousStone()
+
+        return PreciousStone.instance
+
     @property
     def name(self):
         return self.__name 
@@ -54,30 +61,31 @@ class PreciousStone:
         print(f"Name: {self.__name}, Carat: {self.__carat}, Color: {self.__color}, Clarity: {self.__clarity}, Price per carat: {self.__price_per_carat}\n")
     
     def get_total_price(self):
-        """Function from wich we are getting the full price by multiplying carat by price per carat"""
+        """Function from which we are getting the full price by multiplying carat by price per carat"""
         return self.__carat * self.__price_per_carat
     
     def increase_clarity_by_one(self):
-        """The function in wich we are increasing our existing clarity by 1 (one)"""
+        """The function in which we are increasing our existing clarity by 1 (one)"""
         self.__clarity += 1
         return self.__clarity
     
     def increase_price_by_percentage(self, percentage):
-        """The function in wich we are getting an increased price by n-th amount of percents"""
+        """The function in which we are getting an increased price by n-th amount of percents"""
         return self.__clarity * (percentage / 100)
-    
-try:
-    """Throwing a custom exception if stone`s carat amount is lower than 0 (zero)"""
-    new_stone = PreciousStone('Emeraldo', -10, 'Green', 50, 5)
-    if(new_stone.carat < 0):
-        raise Exception("Carat amount cannot be lower than 0")
-    else:
-        new_stone.__str__()
-        print(new_stone.get_total_price(), new_stone.increase_clarity_by_one(), new_stone.increase_price_by_percentage(10))
 
-except Exception as e:
-    if str(e) == "Carat amount cannot be lower than 0":
-        print("Set an appropriate ammount for carat`s parametr")
-    else:
-        new_stone.__str__()
-        print(new_stone.get_total_price(), new_stone.increase_clarity_by_one(), new_stone.increase_price_by_percentage(10))
+if __name__ == "__main__":
+    try:
+        """Throwing a custom exception if stone`s carat amount is lower than 0 (zero)"""
+        new_stone = PreciousStone('Emeraldo', -10, 'Green', 50, 5)
+        if(new_stone.carat < 0):
+            raise Exception("Carat amount cannot be lower than 0")
+        else:
+            new_stone.__str__()
+            print(new_stone.get_total_price(), new_stone.increase_clarity_by_one(), new_stone.increase_price_by_percentage(10))
+
+    except Exception as e:
+        if str(e) == "Carat amount cannot be lower than 0":
+            print("Set an appropriate ammount for carat`s parametr")
+        else:
+            new_stone.__str__()
+            print(new_stone.get_total_price(), new_stone.increase_clarity_by_one(), new_stone.increase_price_by_percentage(10))
