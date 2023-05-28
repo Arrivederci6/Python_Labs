ca5b86c857cf6251eddc2af8389d86835d990cd5
Arrivederci6 <mad060605@gmail.com>
2023-05-28 14:15:43 +0300

    lab3: Level 1 is done. Ver #0.1

--- models/fossil_stone.py
+++ models/fossil_stone.py
@@ -1,18 +1,64 @@
+"""
+Module: fossil_stone.py
+
+This module defines the FossilStone class, which represents
+a fossil stone object.
+
+Attributes:
+- name (str): The name of the stone.
+- color (str): The color of the stone.
+- energy_burned_per_gram (int): The amount of energy burned per gram of the fossil stone.
+- price_per_gram (int): The price per gram of the stone.
+- weight_in_grams (int): The weight of the stone in grams.
+
+Methods:
+- __str__(self):
+    Returns a string representation of the `FossilStone` object.
+
+- get_full_price(self):
+    Calculates and returns the full price of the fossil stone.
+"""
+
 from models.stone import Stone
 
 
 class FossilStone(Stone):
-    def __init__(self, name=0, color=0, energy_burned_per_gram=0, price_per_gram=0, weight_in_grams=0):
-        """Calling the parental constructor"""
+    """
+    This class represents an FossilStone.
+    It defines common properties and methods.
+    """
+    def __init__(self, name=0, color=0, energy_burned_per_gram=0,
+                 price_per_gram=0, weight_in_grams=0):
+        """
+        Initializes an FossilStone object with the specified attributes.
+
+        Args:
+            name (str): The name of the stone.
+            color (str): The color of the stone.
+            energy_burned_per_gram (int): The amount of energy burned per gram
+            of the fossil stone.
+            price_per_gram (int): The price per gram of the stone.
+            weight_in_grams (int): The weight of the stone in grams.
+        """
         super().__init__(name, color, price_per_gram, weight_in_grams)
-        """Creating  objects"""
         self.energy_burned_per_gram = energy_burned_per_gram
 
     def __str__(self):
-        """Overriding the __str___ method to get the full string of objects that we have made"""
-        return f"name: {self.name}, color: {self.color}, energy burned per gram: {self.energy_burned_per_gram}, " \
+        """
+        Returns a string representation of the `FossilStone` object.
+
+        Returns:
+            str: A string representation of the `FossilStone` object.
+        """
+        return f"name: {self.name}, color: {self.color}, " \
+               f"energy burned per gram: {self.energy_burned_per_gram}, " \
                f"price per gram: {self.price_per_gram}, weight in grams: {self.weight_in_grams}"
 
     def get_full_price(self):
-        """Function from which we are getting the full price by multiplying price per gram by energy burned per gram"""
+        """
+        Calculates and returns the full price of the precious stone.
+
+        Returns:
+            int: The full price of the precious stone.
+        """
         return self.price_per_gram * self.energy_burned_per_gram
--- models/precious_stone.py
+++ models/precious_stone.py
@@ -1,46 +1,115 @@
+"""
+Module: precious_stone.py
+
+This module defines the PreciousStone class, which represents a precious stone object.
+
+Attributes:
+- instance (PreciousStone): The singleton instance of the PreciousStone class.
+- name (str): The name of the stone.
+- color (str): The color of the stone.
+- carat (int): The carat amount of the stone.
+- clarity (int): The clarity index of the stone.
+- price_per_carat (int): The price per carat of the stone
+- price_per_gram (int): The price per gram of the stone.
+- weight_in_grams (int): The weight of the stone in grams.
+
+Methods:
+- get_instance():
+    Returns the singleton instance of the `PreciousStone` class.
+
+- __str__(self):
+    Returns a string representation of the `PreciousStone` object.
+
+- get_full_price(self):
+    Calculates and returns the full price of the precious stone.
+
+- increase_clarity_by_one(self):
+    Increases the clarity of the precious stone by one.
+
+- increase_price_by_percentage(self, percentage):
+    Calculates and returns the increased price of the precious stone by a given percentage.
+"""
+
 from models.stone import Stone
 
 
 class PreciousStone(Stone):
-    """ A class that represents precious stone"""
+    """
+    This class represents a PreciousStone.
+    It defines common properties and methods.
+     """
     instance = None
 
-    def __init__(self, name=0, color=0, carat=0, clarity=0, price_per_carat=0, price_per_gram=0, weight_in_grams=0):
-        """Calling the parental constructor"""
+    def __init__(self, name=0, color=0, carat=0, clarity=0, price_per_carat=0,
+                 price_per_gram=0, weight_in_grams=0):
+        """
+        Initializes a `PreciousStone` object with the specified attributes.
+
+        Args:
+            name (str): The name of the stone.
+            color (str): The color of the stone.
+            carat (int): The carat amount of the stone.
+            clarity (int): The clarity amount of the stone.
+            price_per_carat (int): The price per carat of the stone.
+            price_per_gram (int): The price per gram of the stone.
+            weight_in_grams (int): The weight of the stone in grams.
+        """
         super().__init__(name, color, price_per_gram, weight_in_grams)
-        """Args:
-            name: name of the stone.
-            color: color of the stone.
-            carat: stone`s carat amount.
-            clarity: stone`s clarity amount
-            price_per_gram: stone`s price per gram.
-            weight_in_grams: stone`s weight in gram."""
         self.carat = carat
         self.clarity = clarity
         self.price_per_carat = price_per_carat
 
     @staticmethod
-    def get_instance(self):
+    def get_instance():
+        """
+        Returns the singleton instance of the `PreciousStone` class.
+
+        Returns:
+            PreciousStone: The singleton instance of the `PreciousStone` class.
+        """
         if PreciousStone.instance is None:
             PreciousStone.instance = PreciousStone()
 
         return PreciousStone.instance
 
     def __str__(self):
-        """Overriding the __str___ method to get the full string of objects that we have made"""
+        """
+        Returns a string representation of the `PreciousStone` object.
+
+        Returns:
+            str: A string representation of the `PreciousStone` object.
+        """
         return f"name: {self.name}, color: {self.color}, carat: {self.carat}, " \
                f"clarity: {self.clarity}, price per carat: {self.price_per_carat}, " \
                f"price per gram: {self.price_per_gram}, weight in grams: {self.weight_in_grams}"
 
     def get_full_price(self):
-        """Function from which we are getting the full price by multiplying carat by price per carat"""
+        """
+        Calculates and returns the full price of the precious stone.
+
+        Returns:
+            int: The full price of the precious stone.
+        """
         return self.carat * self.price_per_carat
 
     def increase_clarity_by_one(self):
-        """The function in which we are increasing our existing clarity by 1 (one)"""
+        """
+        Increases the clarity of the precious stone by one.
+
+        Returns:
+            int: The new clarity value.
+        """
         self.clarity += 1
         return self.clarity
 
     def increase_price_by_percentage(self, percentage):
-        """The function in which we are getting an increased price by n-th amount of percents"""
+        """
+        Calculates and returns the increased price of the precious stone by a given percentage.
+
+        Args:
+            percentage (float): The percentage value to increase the price.
+
+        Returns:
+            float: The increased price of the precious stone.
+        """
         return self.clarity * (percentage / 100)
